85c9ed68ae62da12161f2641948e2e82009d954e
zsffq999 <zsffq999@163.com>
2015-11-04 18:21:08 +0800

    update matfac

--- asymatfac.py
+++ asymatfac.py
@@ -7,15 +7,16 @@ def AsyMatFac(n, r, numlabel, mu):
 			Tmp = np.dot(PH, QH.T)
 			obj_1 = np.sum(Tmp*Tmp) / r / r
 			obj_2 = n * np.sum((PH[:,l:]+QH[:,l:])**2)
-			print "obj_1: {}, obj_2: {}, ave diff: {}, total: {}".format(obj_1, obj_2, obj_2/(4*n), obj_1+mu*obj_2)
+			print "obj_1: {}, obj_2: {}, ave diff: {}, total: {}".format(obj_1, obj_2, obj_2/(4*n*n), obj_1+mu*obj_2)
 
 	_mu = mu*r*r*n
 	l = numlabel + 1
 	# make label
 	label = np.zeros(n, dtype=np.int32)
 	for i in xrange(numlabel):
-		label[int(float(n)/r*i):int(float(n)/r*(i+1))] = i
-	np.random.shuffle(label)
+		label[int(float(n)/numlabel*i):int(float(n)/numlabel*(i+1))] = i
+	# np.random.shuffle(label)
+	print label
 
 	# make similarity matrix
 	# PH = [P, +H], QH = [Q, -H]
@@ -27,13 +28,13 @@ def AsyMatFac(n, r, numlabel, mu):
 	QH[:,l-1] = -1
 
 	# init: random generate each bit: each bit should balance.
-	idx = np.arange()
+	idx = np.arange(n, dtype=np.int32)
 	for rr in xrange(r):
 		np.random.shuffle(idx)
-		h = np.random.ones(n)
+		h = np.ones(n)
 		h[idx[:n/2]] = -1.0
-		PH[:,:l+rr] = h
-		QH[:,:l+rr] = -h
+		PH[:,l+rr] = h
+		QH[:,l+rr] = -h
 
 	objEvaluate()
 
@@ -59,7 +60,12 @@ def AsyMatFac(n, r, numlabel, mu):
 
 		objEvaluate()
 
+	Hp = PH[:,l:]
+	Hq = -QH[:,l:]
+	np.save('hp.npy', Hp)
+	np.save('hq.npy', Hq)
+	np.save('arglabel.npy', np.argsort(label).squeeze())
+
 
 if __name__ == '__main__':
-	np.random.seed(47)
-	AsyMatFac(5000, 32, 10, 1.0)
+	AsyMatFac(5000, 32, 10, 0)
