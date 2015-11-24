e2de95edbeb1c57b98d9dfeafab82dbabe7ab4d8
zsffq999 <zsffq999@163.com>
2015-11-24 18:37:42 +0800

    Update shle

--- asymatfac.py
+++ asymatfac.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize
 
 
 def AsyMatFac(n, r, numlabel, mu):
@@ -62,10 +63,61 @@ def AsyMatFac(n, r, numlabel, mu):
 
 	Hp = PH[:,l:]
 	Hq = -QH[:,l:]
-	np.save('hp.npy', Hp)
-	np.save('hq.npy', Hq)
-	np.save('arglabel.npy', np.argsort(label).squeeze())
 
+def MatFac(n, r, numlabel):
+	def objEvaluate(flag=1):
+		if flag == 1:
+			Tmp = np.dot(PH, QH.T)
+			obj_1 = np.sum(Tmp*Tmp) / r / r
+			print "obj_1: {}".format(obj_1)
+
+	l = numlabel + 1
+	# make label
+	label = np.zeros(n, dtype=np.int32)
+	for i in xrange(numlabel):
+		label[int(float(n)/numlabel*i):int(float(n)/numlabel*(i+1))] = i
+	np.random.shuffle(label)
+	print label
+
+	# make similarity matrix
+	# PH = [P, +H], QH = [Q, -H]
+	PH = np.zeros((n,l+r), dtype=np.float32)
+	PH[np.arange(n, dtype=np.int32), label] = 1
+	QH = np.copy(PH)
+	PH[:,:l-1] *= 2*r
+	PH[:,l-1] = r
+	QH[:,l-1] = -1
+
+	# init: random generate each bit: each bit should balance.
+	idx = np.arange(n, dtype=np.int32)
+	for rr in xrange(r):
+		np.random.shuffle(idx)
+		h = np.ones(n)
+		h[idx[n/2]] = -1
+		#h = np.where(np.random.rand(n)>=0.5, 1, -1)
+		PH[:,l+rr] = h
+		QH[:,l+rr] = -h
+
+	objEvaluate()
+
+	# do optimize loops
+	# optimize relaxed function using coordinate descent
+	fun = lambda x: -np.dot(np.dot(x, PH), np.dot(x, QH))
+	gra = lambda x: -2*np.dot(PH, np.dot(x, QH))
+	bnds = [(-1,1) for i in xrange(n)]
+	h = np.zeros(n)
+	for rr in xrange(r):
+		h[:] = PH[:,l+rr]
+		PH[:,l+rr] = QH[:,l+rr] = 0
+		res = minimize(fun, h, method='L-BFGS-B', jac=gra, bounds=bnds, options={'disp': False})
+		h = np.where(res.x>=0, 1, -1)
+		PH[:,l+rr] = h
+		QH[:,l+rr] = -h
+		objEvaluate()
+
+	#print PH[:,l:]
+	#print (r - np.dot(PH[:,l:], PH[:,l:].T))/2
 
 if __name__ == '__main__':
-	AsyMatFac(5000, 32, 10, 0)
+	# AsyMatFac(5000, 32, 10, 0)
+	MatFac(50000, 32, 10)
