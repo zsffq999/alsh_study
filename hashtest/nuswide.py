1b917450d86d7a19e4cb470c029e07c245da3211
zsffq999 <zsffq999@163.com>
2015-12-14 17:13:13 +0800

    Update nuswide loader

--- hashtest/nuswide.py
+++ hashtest/nuswide.py
@@ -13,9 +13,15 @@ def dat2npy(src, dst):
 
 def make_labels():
 	label_file = sorted(os.listdir('TrainTestLabels'))
+	train_labels = []
+	test_labels = []
 	for i in xrange(81):
 		trn_file = label_file[i*2]
 		tst_file = label_file[i*2+1]
+		with open('TrainTestLabels'+trn_file, 'r') as f:
+			pass
+		with open('TrainTestLabels'+tst_file, 'r') as f:
+			pass
 
 
 if __name__ == "__main__":
