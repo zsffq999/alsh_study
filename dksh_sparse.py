1a73720b0b0b0bb6ac39e4bb186b399989fb9d56
zsffq999 <zsffq999@163.com>
2015-12-28 14:27:04 +0800

    make sim_mat sparse

--- dksh_sparse.py
+++ dksh_sparse.py
@@ -4,9 +4,10 @@ from scipy.linalg import eigh
 from ksh import RBF
 import time
 from scipy.optimize import minimize
+from scipy.sparse import csc_matrix, csr_matrix
 
 
-class DKSHv2(object):
+class Sparse_DKSH(object):
 	def __init__(self, r, m, numlabel, kernel):
 		self.r = r # num of hash bits
 		self.m = m # num of anchors
@@ -17,8 +18,7 @@ class DKSHv2(object):
 		self.mvec = None # mean vector
 
 		# Hash code and out-of-sample labels
-		self.Hp = None # database hash code
-		self.Hq = None # query hash code
+		self.H = None
 		self.trainlabel = None
 
 		# tuning parameters
@@ -45,25 +45,20 @@ class DKSHv2(object):
 		self.mvec = np.mean(KK, axis=0).reshape((1, self.m))
 		KK = KK - self.mvec
 
-		# pairwise label matrix, rS=PQ^T
-		l = self.numlabel + 1
-
-		# PH = [P, +Hp], QH = [Q, -Hq]
-		PH = np.zeros((n,l+self.r), dtype=np.float32)
+		# pairwise label matrix, S = 2*P*P.T-1_{n*n}
 		if len(trainlabel.shape) >= 2:
 			assert trainlabel.shape[1] == self.numlabel
-			PH[:,:self.numlabel] = trainlabel
+			P = csc_matrix(trainlabel, dtype=np.float32)
+			P = P.T
 		else:
-			PH[np.arange(n, dtype=np.int32), trainlabel] = 1
-		QH = np.copy(PH)
-		PH[:,:l-1] *= 2 * self.r
-		PH[:,l-1] = self.r
-		QH[:,l-1] = -1
+			P = csc_matrix((np.ones(n),[np.arange(n, dtype=np.int32), trainlabel]), shape=(n,self.numlabel), dtype=np.float32)
+			P = P.T
+		H = np.zeros((n,self.r))
 
 		# projection optimization
 		RM = np.dot(KK.T, KK)
 		W = np.zeros((self.m, self.r), dtype=np.float32) # parameter W
-		LM = np.dot(np.dot(KK.T, PH[:,:l]), np.dot(QH.T[:l], KK))
+		LM = self.r*(2*np.dot(P.dot(KK).T, P.dot(KK)) - np.dot(np.sum(KK.T, axis=1, keepdims=True), np.sum(KK, axis=0, keepdims=True)))
 
 		# Evaluator
 		evaor = ObjEvaluate(0)
@@ -72,8 +67,8 @@ class DKSHv2(object):
 		# step 1.1: batch coordinate optimization
 		h0 = np.zeros(n)
 		print '\nSTEP 1: Initialize with spectral relaxation...'
+		print 'step 1.1...'
 		for rr in range(self.r):
-
 			if rr > 0:
 				tmp = np.dot(KK.T, h0.reshape((n,1)))
 				LM -= np.dot(tmp, tmp.T)
@@ -83,14 +78,14 @@ class DKSHv2(object):
 			W[:,rr] *= np.sqrt(n/tmp)
 
 			h0 = np.where(np.dot(KK, W[:,rr]) >= 0, 1, -1)
-			PH[:,l+rr] = h0
-			QH[:,l+rr] = -1 * h0
-		evaor(PH, QH, W, KK, mu, self.lmda, self.r, 1)
+			H[:,rr] = h0
+		evaor(P, H, W, KK, mu, self.lmda, self.r, 1)
 
 		# step 1.2: batch coordinate optimization for some loops
 		for t in range(5):
+			print 'step 1.{}...'.format(t+2)
 			for rr in range(self.r):
-				h0[:] = PH[:,l+rr]
+				h0[:] = H[:,rr]
 				tmp = np.dot(KK.T, h0.reshape((n,1)))
 				LM += np.dot(tmp, tmp.T)
 
@@ -104,9 +99,11 @@ class DKSHv2(object):
 				tmp = np.dot(KK.T, h0.reshape((n,1)))
 				LM -= np.dot(tmp, tmp.T)
 
-				PH[:,l+rr] = h0
-				QH[:,l+rr] = -1 * h0
-		evaor(PH, QH, W, KK, mu, self.lmda, self.r, 1)
+				H[:,rr] = h0
+
+				# PH[:,l+rr] = h0
+				# QH[:,l+rr] = -1 * h0
+		evaor(P, H, W, KK, mu, self.lmda, self.r, 1)
 		# TSP
 		# PH[:,l:] = np.where(np.random.rand(n,self.r)>0.5,1,-1)
 		# QH[:,l:] = -PH[:,l:]
@@ -121,31 +118,28 @@ class DKSHv2(object):
 		for t in range(5):
 			print '\nIter No: %d' % t
 			# step 2.1: fix Hp, Hq, optimize W
-			W = -np.dot(invRM, np.dot(KK.T, QH[:,l:]))
-			evaor(PH, QH, W, KK, mu, self.lmda, self.r, 2)
+			W = np.dot(invRM, np.dot(KK.T, H))
+			# W = -np.dot(invRM, np.dot(KK.T, QH[:,l:]))
+			evaor(P, H, W, KK, mu, self.lmda, self.r, 2)
 
 			# step 2.2: fix W, optimize H
 			KK_W = np.dot(KK, W)
 			for rr in range(self.r):
-				h[:] = PH[:,l+rr]
-				QH[:,l+rr] = PH[:,l+rr] = 0
-				fun = lambda x: -np.dot(np.dot(x,PH), np.dot(x,QH)) - mu * np.dot(KK_W[:,rr], x)
-				gra = lambda x: -2*np.dot(PH, np.dot(x, QH)) - mu * KK_W[:,rr]
-				res = minimize(fun, h, method='L-BFGS-B', jac=gra, bounds=bnds, options={'disp': False})
+				if (rr+1) % 10 == 0:
+					print 'rr:', rr
+				h[:] = H[:,rr]
+				H[:,rr] = 0
+				fun = lambda x: -self.r*(2*np.sum(P.dot(x)**2) - np.sum(x)**2) + np.sum(np.dot(x,H)**2) - mu * np.dot(KK_W[:,rr], x)
+				gra = lambda x: -2*self.r*(2*P.T.dot(P.dot(x)) - np.sum(x)) + 2*np.dot(H,np.dot(x,H)) - mu * KK_W[:,rr]
+				res = minimize(fun, h, method='L-BFGS-B', jac=gra, bounds=bnds, options={'disp': False, 'maxiter':500, 'maxfun':500})
 				h[:] = np.where(res.x>=0, 1, -1)
-				PH[:,l+rr] = h
-				QH[:,l+rr] = -1 * h
+				H[:,rr] = h
 
-			evaor(PH, QH, W, KK, mu, self.lmda, self.r, 1)
+			evaor(P, H, W, KK, mu, self.lmda, self.r, 1)
 
 		self.W = W
 		self.trainlabel = trainlabel
-		self.Hp = np.copy(PH[:,l:])
-		self.Hq = np.copy(-QH[:,l:])
-
-		QH[:,l:] = np.where(np.dot(KK, self.W)>=0, -1, 1)
-		PH[:,l:] = -QH[:,l:]
-		evaor(PH, QH, W, KK, mu, self.lmda, self.r, 1)
+		self.H = np.copy(H)
 
 	def queryhash(self, qdata):
 		Kdata = self.kernel(qdata, self.anchors)
@@ -164,16 +158,16 @@ class ObjEvaluate(object):
 		self.Q2 = 0
 		self.flag = flag
 
-	def __call__(self, PH, QH, W, KK, mu, lamb, r, flag=0):
+	def __call__(self, P, H, W, KK, mu, lamb, r, flag=0):
 		if self.flag == 0 or flag == 0: # do not compute
 			return
 		elif flag == 1: # update all
-			Tmp1 = np.dot(PH, QH.T)
+			Tmp1 = (2*r)*P.T.dot(P).toarray()-r*np.ones((len(H),len(H))) - np.dot(H,H.T)
 			self.Q1 = np.sum(np.sum(Tmp1*Tmp1,axis=1)) / (r**2)
-			Tmp2 = np.dot(KK, W) + QH[:,-r:]
+			Tmp2 = np.dot(KK, W) - H
 			self.Q2 = (np.sum(np.sum(Tmp2*Tmp2,axis=1)) + lamb*np.sum(np.sum(W*W,axis=1)))
 		else:
-			Tmp2 = np.dot(KK, W) + QH[:,-r:]
+			Tmp2 = np.dot(KK, W) - H
 			self.Q2 = (np.sum(np.sum(Tmp2*Tmp2,axis=1)) + lamb*np.sum(np.sum(W*W,axis=1)))
 		print 'Obj Value: Q1={0}, Q2={1}, Total={2}'.format(self.Q1, self.Q2, self.Q1+mu*self.Q2)
 
@@ -183,15 +177,15 @@ def test():
 	X = np.load('data/cifar_gist.npy')
 	Y = np.load('data/cifar_label.npy')
 
-	traindata = X[:59000]
-	trainlabel = Y[:59000]
+	traindata = X[:5000]
+	trainlabel = Y[:5000]
 	basedata = X[:59000]
 	baselabel = Y[:59000]
 	testdata = X[59000:]
 	testlabel = Y[59000:]
 
 	# train model
-	dksh = DKSHv2(32, 1000, 10, RBF)
+	dksh = Sparse_DKSH(32, 1000, 10, RBF)
 	tic = time.clock()
 	dksh.train(traindata, trainlabel)
 	toc = time.clock()
---
+++ ksh_sparse.py
@@ -0,0 +1,212 @@
+import numpy as np
+from data import hash_value, hash_evaluation
+from scipy.linalg import eigh
+from scipy.sparse import csc_matrix, csr_matrix
+import time
+
+
+def OptProjectionFast(r, K, P, H, a0, cn):
+	(n, m) = K.shape
+	cost = np.zeros(cn+2, dtype=np.float32)
+	y = np.dot(K, a0)
+	y = 2.0 / (1+np.exp(-1*y)) - 1
+	cost[0] = -r*(2*np.sum(P.dot(y)**2) - np.sum(y)**2) + np.sum(np.dot(y,H)**2)
+	# cost[0] = -np.dot(np.dot(y.T,S), y)
+	cost[1] = cost[0]
+
+	a1 = a0 
+	delta = np.zeros(cn+2)
+	delta[0] = 0
+	delta[1] = 1
+
+	beta = np.zeros(cn+1)
+	beta[0] = 1
+
+	for t in range(0, cn):
+		alpha = (delta[t]-1)/delta[t+1]
+		v = a1 + alpha*(a1-a0)
+		y0 = np.dot(K, v)
+		y1 = 2.0 / (1+np.exp(-1*y0)) - 1
+		# gv = -np.dot(np.dot(y1.T,PH), np.dot(QH.T,y1))
+		gv = -r*(2*np.sum(P.dot(y1)**2) - np.sum(y1)**2) + np.sum(np.dot(y1,H)**2)
+		# ty = np.dot(PH, np.dot(QH.T,y1)) * (np.ones(n)-y1**2)
+		ty = (r*(2*P.T.dot(P.dot(y1)) - np.sum(y1)) - 2*np.dot(H,np.dot(y1,H))) * (np.ones(n)-y1**2)
+		dgv = -np.dot(K.T, ty)
+
+		# seek beta
+		flag = 0
+		for j in range(0,51):
+			b = 2**j * beta[t]
+			z = v-dgv/b
+			y0 = np.dot(K,z)
+			y1 = 2.0 / (1+np.exp(-1*y0)) - 1
+			gz = -r*(2*np.sum(P.dot(y1)**2) - np.sum(y1)**2) + np.sum(np.dot(y1,H)**2)
+			# gz = -np.dot(np.dot(y1.T, S), y1)
+			dif = z-v
+			gvz = gv + np.dot(dgv.T, dif) + np.dot(b*dif.T, dif)/2
+
+			if gz <= gvz:
+				flag = 1
+				beta[t+1] = b
+				a0 = a1 
+				a1 = z
+				cost[t+2] = gz
+				break
+
+		if flag == 0:
+			# t = t-1
+			break
+		else:
+			delta[t+2] = (1+np.sqrt(1+4*delta[t+1]**2))/2
+
+		if abs(cost[t+2] - cost[t+1])/n <= 1e-2:
+			break
+
+	a = a1 
+	cost = cost/n
+	return a, cost
+
+
+class KSH(object):
+	def __init__(self, r, m, numlabel, kernel):
+		self.r = r # num of hash bits
+		self.m = m # num of anchors
+		self.kernel = kernel # kernel function
+		self.anchors = None # anchor points
+		self.A = None # parameter to optimize
+		self.numlabel = numlabel
+		self.mvec = None # mean vector
+
+	def train(self, traindata, trainlabel):
+
+		# for debugging
+		def evaObjective(flag=1):
+			if flag == 1:
+				Tmp = (2*self.r)*P.T.dot(P).toarray()-self.r*np.ones((n,n)) - np.dot(H,H.T)
+				obj = np.sum(np.sum(Tmp*Tmp, axis=1)) / (self.r**2)
+				print 'Obj Value: {}'.format(obj)
+
+		n = len(traindata)
+		# shuffle data
+		indexes = np.arange(n, dtype=np.int32)
+		np.random.shuffle(indexes)
+		traindata = traindata[indexes]
+		trainlabel = trainlabel[indexes]
+
+		# determine anchors
+		anchoridx = np.copy(indexes)
+		np.random.shuffle(anchoridx)
+		anchoridx = anchoridx[:self.m]
+		self.anchors = traindata[anchoridx]
+
+		# kernel matrix and mean
+		KK = self.kernel(traindata, self.anchors)
+		self.mvec = np.mean(KK, axis=0).reshape((1, self.m))
+		KK = KK - self.mvec
+
+		# pairwise label matrix, S = 2*P*P.T-1_{n*n}
+		if len(trainlabel.shape) >= 2:
+			assert trainlabel.shape[1] == self.numlabel
+			P = csc_matrix(trainlabel, dtype=np.float32)
+			P = P.T
+		else:
+			P = csc_matrix((np.ones(n),[np.arange(n, dtype=np.int32), trainlabel]), shape=(n,self.numlabel), dtype=np.float32)
+			P = P.T
+		H = np.zeros((n,self.r))
+
+		# projection optimization
+		RM = np.dot(KK.T, KK)
+		A = np.zeros((self.m, self.r), dtype=np.float32) # parameter W
+		LM = self.r*(2*np.dot(P.dot(KK).T, P.dot(KK)) - np.dot(np.sum(KK.T, axis=1, keepdims=True), np.sum(KK, axis=0, keepdims=True)))
+		print LM, np.sum(LM)
+
+		evaObjective()
+
+		# greedy optimization
+		for rr in range(0, self.r):
+			print "No:", rr
+
+			# step 1: spectral relaxation
+			if rr > 0:
+				tmp = np.dot(KK.T, h0.reshape((n,1)))
+				LM -= np.dot(tmp, tmp.T)
+			(V, U) = eigh(LM, RM, eigvals_only=False)
+			A[:,rr] = U[:,self.m-1]
+			tmp = np.dot(np.dot(A[:,rr].T, RM), A[:,rr])
+			A[:,rr] *= np.sqrt(n/tmp)
+
+
+			# step 2: sigmoid smoothing
+			get_vec, cost = OptProjectionFast(self.r, KK, P, H[:,:rr], A[:,rr], 500)
+
+			h0 = np.dot(KK, A[:,rr])
+			h0 = np.where(h0>=0, 1, -1)
+
+			h1 = np.dot(KK, get_vec)
+			h1 = np.where(h1>=0, 1, -1)
+
+			if -self.r*(2*np.sum(P.dot(h1)**2) - np.sum(h1)**2) + np.sum(np.dot(h1,H[:,:rr])**2) > \
+				-self.r*(2*np.sum(P.dot(h0)**2) - np.sum(h0)**2) + np.sum(np.dot(h0,H[:,:rr])**2):
+				A[:,rr] = get_vec
+				h0 = h1
+
+			# update H
+			H[:,rr] = h0[:]
+
+			evaObjective()
+
+		self.A = A
+
+	def queryhash(self, qdata):
+		Kdata = self.kernel(qdata, self.anchors)
+		Kdata -= self.mvec
+		Y = np.dot(Kdata, self.A)
+		Y = np.where(Y>=0, 1, 0)
+		return hash_value(Y)
+
+	def basehash(self, data):
+		return self.queryhash(data)
+
+
+def RBF(X, Y):
+	lenX = X.shape[0]
+	lenY = Y.shape[0]
+	X2 = np.dot(np.sum(X * X, axis=1).reshape((lenX, 1)), np.ones((1, lenY), dtype=np.float32))
+	Y2 = np.dot(np.ones((lenX, 1), dtype=np.float32), np.sum(Y * Y, axis=1).reshape((1, lenY)))
+	return np.exp(2*np.dot(X,Y.T) - X2 - Y2)
+
+
+def test():
+	np.random.seed(47)
+	X = np.load('data/cifar_gist.npy')
+	Y = np.load('data/cifar_label.npy')
+	idx = np.arange(60000, dtype=np.int32)
+	np.random.shuffle(idx)
+	X = X[idx]
+	Y = Y[idx]
+	traindata = X[:5000]
+	trainlabel = Y[:5000]
+	basedata = X[:59000]
+	baselabel = Y[:59000]
+	testdata = X[59000:]
+	testlabel = Y[59000:]
+
+	# make labels
+	gnd_truth = np.array([y == baselabel for y in testlabel]).astype(np.int8)
+
+	# train model
+	ksh = KSH(32, 1000, 10, RBF)
+	tic = time.clock()
+	ksh.train(traindata, trainlabel)
+	# ksh = ksh2.KSH(5000, 300, 12, traindata, trainlabel, RBF)
+	toc = time.clock()
+	print 'time:', toc-tic
+	H_base = ksh.basehash(basedata)
+	H_test = ksh.queryhash(testdata)
+
+	# evaluate
+	res = hash_evaluation(H_test, H_base, gnd_truth, 59000)
+	print 'MAP:', res['map']
+
+if __name__ == "__main__":
+	test()
