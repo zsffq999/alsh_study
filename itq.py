5766defa0fa0fee509ea5fb7763a548916eeec5e
zsffq999 <zsffq999@163.com>
2016-01-04 17:21:48 +0800

    update itq

--- itq.py
+++ itq.py
@@ -12,6 +12,7 @@ class ITQ(object):
 		self.reg = reg
 		self.l = l
 		self.W = None
+		self.R = None
 
 	def train(self, traindata, trainlabel=None):
 		n = len(traindata)
@@ -19,31 +20,20 @@ class ITQ(object):
 		if self.type == 'kernel':
 			pass
 
-		# execute PCA first
+		# centering data
 		self.mean = np.mean(traindata, axis=0).reshape((1,traindata.shape[1]))
-		S = np.cov(traindata.T)
-		self.Evecs, _, _ = np.linalg.svd(S)
-		self.Evecs = self.Evecs[:,:self.r]
 
-		# encode traindata
-		V = np.dot(traindata-self.mean, self.Evecs)
-
-		# PCA-ITQ
 		if self.type == 'pca':
-			R = np.random.normal(size=(self.r, self.r))
-			R, _, _ = np.linalg.svd(R)
-			for i in xrange(self.n_iter):
-				Z = np.dot(V, R)
-				UX = np.where(Z>=0, 1, -1)
-				C = np.dot(UX.T, V)
-				UB, _, UA = np.linalg.svd(C)
-				R = np.dot(UA, UB.T)
-			self.W = R
+			# PCA
+			S = np.cov(traindata.T)
+			Evecs, _, _ = np.linalg.svd(S)
+			self.W = Evecs[:,:self.r]
 		elif self.type == 'cca' or self.type == 'kernel':
+			# CCA
 			# make label vector
 			Y = np.zeros((n,self.l), dtype=np.float32)
 			Y[np.arange(n, dtype=np.int32), trainlabel] = 1
-			z = np.hstack((traindata, Y))
+			z = np.hstack((traindata-self.mean, Y))
 			C = np.cov(z.T)
 
 			sx = traindata.shape[1]
@@ -59,19 +49,31 @@ class ITQ(object):
 			Z = 0.5*(Z+Z.T)
 
 			r, Wx = np.linalg.eig(Z)   # basis in h (X)
-			r = np.sqrt(np.real(r)) # as the original r we get is lamda^2
+			r = np.real(r)
+			r = np.sqrt(np.where(r>=0, r, 0)) # as the original r we get is lamda^2
 			Wx = np.dot(invRx, Wx)   # actual Wx values
 
 			index = np.argsort(r)[::-1]
-			Wx = Wx[:,index]
-			r = r[index]
-			print Wx.shape
+			Wx = Wx[:,index[:self.r]]
+			r = r[index[:self.r]]
 			self.W = Wx * r.reshape((1,self.r))
+
+		# ITQ
+		V = np.dot(traindata-self.mean, self.W)
+		R = np.random.normal(size=(self.r, self.r))
+		R, _, _ = np.linalg.svd(R)
+		for i in xrange(self.n_iter):
+			Z = np.dot(V, R)
+			UX = np.where(Z>=0, 1, -1)
+			C = np.dot(UX.T, V)
+			UB, _, UA = np.linalg.svd(C)
+			R = np.dot(UA, UB.T)
+		self.R = R
 
 	def queryhash(self, qdata):
 		if self.type != 'kernel':
-			Kdata = np.dot(qdata-self.mean, self.Evecs)
-			Y = np.dot(Kdata, self.W)
+			Kdata = np.dot(qdata-self.mean, self.W)
+			Y = np.dot(Kdata, self.R)
 			Y = np.where(Y>=0, 1, 0)
 			return hash_value(Y)
 		else:
