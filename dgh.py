01e4fa94366efd1b499dd3fbfe088113b40179a4
zsffq999 <zsffq999@163.com>
2016-01-20 17:15:31 +0800

    update dgh

--- dgh.py
+++ dgh.py
@@ -18,9 +18,14 @@ class DGH(object):
 		self.nnanchors = nnanchors
 		self.sigma = sigma
 		self.n_iter = 5
-		self.rou = 1
+		self.rou = 5
 
 	def train(self, traindata, trainlabel=None):
+		def obj_value(flag=1):
+			if flag == 1:
+				obj_B = np.trace(np.dot(Z.T.dot(B).T, np.multiply(Lamb.T, (Z.T.dot(B)))))
+				obj_Y = np.sum(np.multiply(B,Y))
+				print 'obj_B:', obj_B, ', obj_Y:', obj_Y, 'total:', obj_B+self.rou*obj_Y
 		n = len(traindata)
 		# shuffle data
 		indexes = np.arange(n, dtype=np.int32)
@@ -34,14 +39,13 @@ class DGH(object):
 		self.anchors = traindata[anchoridx]
 
 		(Z, sigma) = self._Z(traindata, self.anchors, self.nnanchors, self.sigma)
-		print Z.shape
 		Lamb = 1.0 / Z.sum(axis=0).reshape((1,self.m))
 
 		# Number of iterations: T_R=100, T_B=300, T_G=20
 		Tr = 100
 		Tb = 300
-		Tg = 20
-
+		Tg = 0
+		
 		# init: DGH-R
 		# Compute A = P \Phi P.T
 		# A = (Z \Lamb^-0.5)(Z \Lamb^-0.5).T
@@ -53,26 +57,42 @@ class DGH(object):
 		theta = (s*s)[1:].reshape((1,self.r))
 
 		B = np.where(H>=0, 1, -1)
-
+		
+		R = np.eye(self.r)
+		
+		
 		# DGH-R
 		for _iter in xrange(Tr):
 			_U, _, _VT = scipy.linalg.svd(np.dot((H*theta).T, B))
 			R = np.dot(_U, _VT)
 			B = np.where(np.dot(H*theta, R)>=0, 1, -1)
+		
 		Y = np.sqrt(n) * np.dot(H, R)
-
+		
+		obj_value()
+		
 		# DGH algorithm
 		for _iter in xrange(Tg):
+			print 'iter', _iter, '...'
 			# B-subproblem
 			for i in xrange(Tb):
 				Tmp = 2*Z.dot(np.multiply(Lamb.T, (Z.T.dot(B)))) + self.rou*Y
-				Tmp = np.where(Tmp==0.0, B, Tmp)
+				Tmp = np.where(np.abs(Tmp)<=1e-4, B, Tmp)
 				tB = np.where(Tmp>=0, 1, -1)
 				if np.max(np.abs(tB-B)) < 1e-4:
 					break
 				B = tB
+			obj_value()
 			# Y-subproblem
 			B1 = np.sum(B.T, axis=1, keepdims=True)
+			
+			JB = B - 1.0 / n * np.dot(np.ones((n,1)), np.sum(B, axis=0, keepdims=True))
+			U, Sigma, VT = scipy.linalg.svd(JB, full_matrices=False, overwrite_a=True)
+			V = VT.T
+			ind = self.r - np.searchsorted(Sigma[::-1], 1e-4)
+			Y = np.dot(U, V.T) * np.sqrt(n)
+			
+			'''
 			BJB = np.dot(B.T,B) - 1.0/n * np.dot(B1, B1.T)
 			V, Sigma2, _ = scipy.linalg.svd(BJB)
 			ind = self.r - np.searchsorted(Sigma2[::-1], 1e-5)
@@ -81,20 +101,26 @@ class DGH(object):
 			BVSigma = np.dot(B, V[:,:ind]*inv_sigma)
 			U = np.zeros((n,self.r))
 			U[:,:ind] = BVSigma - np.dot(np.ones((n,1)), np.sum(BVSigma, axis=0, keepdims=True))
+			print np.dot(U[:,:ind].T, U[:,:ind])
 			U[:,ind:] = np.random.rand(n,self.r-ind)
-			U[:,ind:] = scipy.linalg.qr(U)[0][:,ind:]
-			Y = np.dot(U, V.T) / np.sqrt(n)
+			U[:,ind:] = scipy.linalg.qr(U, mode='economic')[0][:,ind:]
+			Y = np.dot(U, V.T) * np.sqrt(n)
+			'''
+			
+			obj_value()
 
-		self.W = (Z.T.dot(B)).T * Lamb
+		self.W = np.multiply((Z.T.dot(B)).T, Lamb).T
+		self.B = B
 
 	def queryhash(self, data):
 		(Z, _) = self._Z(data, self.anchors, self.nnanchors, self.sigma)
+		print Z.shape
 		Y = Z.dot(self.W)
 		Y = np.where(Y>=0, 1, 0)
 		return hash_value(Y)
 
 	def basehash(self, data):
-		return self.queryhash(data)
+		return self.B
 
 	@staticmethod
 	def _Z(data, anchors, nnanchors, sigma):
@@ -159,15 +185,15 @@ if __name__ == '__main__':
 	X = np.load('data/cifar_gist.npy')
 	Y = np.load('data/cifar_label.npy')
 
-	traindata = X[:5000]
-	trainlabel = Y[:5000]
+	traindata = X[:59000]
+	trainlabel = Y[:59000]
 	basedata = X[:59000]
 	baselabel = Y[:59000]
 	testdata = X[59000:]
 	testlabel = Y[59000:]
 
 	# train model
-	dksh = DGH(32, 1000)
+	dksh = DGH(48, 300)
 	tic = time.clock()
 	dksh.train(traindata)
 	toc = time.clock()
@@ -175,6 +201,9 @@ if __name__ == '__main__':
 
 	H_test = dksh.queryhash(testdata)
 	H_base = dksh.queryhash(basedata)
+	
+	idx = np.argsort(baselabel)
+	print H_base[idx[0:59000:3000]]
 
 	# make labels
 	gnd_truth = np.array([y == baselabel for y in testlabel]).astype(np.int8)
