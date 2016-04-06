41754b9f06ceb59bc9606fdcc5acabebd30ce20c
zsffq999 <zsffq999@163.com>
2016-04-06 20:55:50 +0800

    update bqp

--- bqp.py
+++ bqp.py
@@ -84,15 +84,20 @@ class AMF_BQP(BQP):
 		if self.b < 0:
 			d1[:,self.Y.shape[1]] = -d1[:,self.Y.shape[1]]
 		d1[:,self.Y.shape[1]+1:] = -d1[:,self.Y.shape[1]+1:]
-		_s, _v = np.linalg.eig(np.dot(v.reshape((v.shape[0],1))*d, (v.reshape((v.shape[0],1))*d1).T))
+		_v, _s = np.linalg.eig(np.dot(v.reshape((v.shape[0],1))*d, (v.reshape((v.shape[0],1))*d1).T))
 		# s1, v1, d1 = np.linalg.svd(2*r*Y.dot(Y.T).toarray()-r-np.dot(H,H.T))
-		return np.dot(s, _s[:,:3])
+		ind = np.argmax(_v)
+		# print np.min(_v)
+		# print _v
+		return np.dot(s, _s[:,ind])
 
 	def col_sum(self, col_ind):
-		return np.dot(self.P, np.sum(self.Q[col_ind], axis=0))
+		n_tmp = len(col_ind)
+		Y_tmp = self.Y[col_ind].sum(axis=0).A[0]
+		return self.a*self.Y.dot(Y_tmp) + self.b * n_tmp
 
 	def diag(self):
-		return self.a*np.sum(self.Y*self.Y, axis=1) + self.b
+		return (self.a*self.Y.multiply(self.Y).sum(axis=1)).A[:,0] + self.b
 
 
 def bqp_relax(bqp, init=None):
@@ -101,7 +106,7 @@ def bqp_relax(bqp, init=None):
 	:return: optimization value
 	'''
 	n = bqp.size()
-	if init == None:
+	if None == init:
 		init = np.where(np.random.rand(n)>0.5, 1, -1)
 	bnds = [(-1,1) for i in xrange(n)]
 	res = minimize(bqp.neg_obj, init, method='L-BFGS-B', jac=bqp.neg_grad, bounds=bnds, options={'disp': False, 'maxiter':500, 'maxfun':500})
@@ -114,10 +119,8 @@ def bqp_spec(bqp, init=None):
 	:return:
 	'''
 	vs = bqp.max_eigen()
-	print bqp.neg_obj(vs[:,0])
-	print bqp.neg_obj(vs[:,1])
-	print bqp.neg_obj(vs[:,2])
-	return np.where(vs[:,0]>0, 1, -1)
+	# print bqp.neg_obj(vs)
+	return np.where(vs>0, 1, -1)
 
 
 def bqp_cluster(bqp, init=None):
@@ -127,13 +130,13 @@ def bqp_cluster(bqp, init=None):
 	'''
 	n = bqp.size()
 	maxiter = 10
-	q = 0.5*bqp.q - bqp.col_sum(slice(n))
+	q = 0.5*bqp.q - bqp.col_sum(range(n))
 	diag = bqp.diag()
-	lamb = n
+	lamb = 0
 	lamb_diag = lamb - diag
 
 	# initialize cluster
-	if init == None:
+	if None == init:
 		cluster = np.argwhere(np.random.rand(n)>0.5)[:,0]
 	else:
 		cluster = np.argwhere(init==1)[:,0]
@@ -142,9 +145,10 @@ def bqp_cluster(bqp, init=None):
 	for i in xrange(maxiter):
 		dist_to_cluster = bqp.col_sum(cluster) + 0.5*q
 		dist_to_cluster[cluster] += lamb_diag[cluster]
+		# print i, dist_to_cluster
 		med = np.median(dist_to_cluster)
 		new_cluster = np.argwhere(dist_to_cluster>med)[:,0]
-		if cluster == new_cluster:
+		if np.all(cluster == new_cluster):
 			break
 		cluster = new_cluster
 	res = -np.ones(n)
@@ -153,27 +157,31 @@ def bqp_cluster(bqp, init=None):
 
 
 if __name__ == '__main__':
-	n = 500
+	n = 5000
 	numlabel = 10
 	r = 32
 	trainlabel = np.random.randint(0,10, size=n)
 	Y = csc_matrix((np.ones(n),[np.arange(n, dtype=np.int32), trainlabel]), shape=(n,10), dtype=np.float32)
-	H = np.where(np.random.rand(n,r)>0.5,1,-1)
+	H = np.where(np.random.rand(n,r)>0.5, 1, -1)
 
 	def obj():
 		Tmp = 2*Y.dot(Y.T).toarray()-1-np.dot(H,H.T)/float(r)
 		return np.sum(np.sum(Tmp*Tmp, axis=1), axis=0)
 
 	h = np.zeros(n)
-	for t in xrange(5):
+	import time
+	tic = time.clock()
+	for t in xrange(3):
 		for rr in xrange(r):
 			h[:] = H[:,rr]
 			H[:,rr] = 0
 			bqp = AMF_BQP(Y, 2*r, -r, H)
-			h1 = bqp_spec(bqp, h)
-			print h1
-			if bqp.neg_obj(h1) < bqp.neg_obj(h):
+			h1 = bqp_cluster(bqp, h)
+			# print h1
+			if bqp.neg_obj(h1) < bqp.neg_obj(h) or t == 0:
 				H[:,rr] = h1
 			else:
 				H[:,rr] = h
-			print 'Iter {}, step {}: obj={}'.format(t, rr, obj())
+			# print 'iter: {}, rr: {}, obj: {}'.format(t, rr, obj())
+	toc = time.clock()
+	print 'obj: {}; time: {}'.format(obj(), toc-tic)
